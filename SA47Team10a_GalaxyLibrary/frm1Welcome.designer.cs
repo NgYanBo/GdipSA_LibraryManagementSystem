0333f32791537cab7263fd987927e92eab5e00b2
Ng Yan Bo <e0321109@u.nus.edu>
2019-02-17 20:17:58 +0800

    First and Final commit for Team10A Project :)

---
+++ SA47Team10a_GalaxyLibrary/frm1Welcome.designer.cs
@@ -0,0 +1,134 @@
+﻿namespace SA47Team10a_GalaxyLibrary
+{
+    partial class frm1Welcome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm1Welcome));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLibraryUsers = new System.Windows.Forms.Button();
+            this.btnStaffLogin = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1623, 1177);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(488, 666);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(693, 66);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Welcome to Galaxy Library";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(345, 733);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(976, 66);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "A Vast Collection Of Knowledge Awaits";
+            // 
+            // btnLibraryUsers
+            // 
+            this.btnLibraryUsers.BackColor = System.Drawing.Color.Indigo;
+            this.btnLibraryUsers.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLibraryUsers.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnLibraryUsers.Location = new System.Drawing.Point(483, 841);
+            this.btnLibraryUsers.Name = "btnLibraryUsers";
+            this.btnLibraryUsers.Size = new System.Drawing.Size(330, 94);
+            this.btnLibraryUsers.TabIndex = 0;
+            this.btnLibraryUsers.Text = "Library Users";
+            this.btnLibraryUsers.UseVisualStyleBackColor = false;
+            this.btnLibraryUsers.Click += new System.EventHandler(this.btnLibraryUsers_Click);
+            // 
+            // btnStaffLogin
+            // 
+            this.btnStaffLogin.BackColor = System.Drawing.Color.Indigo;
+            this.btnStaffLogin.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnStaffLogin.FlatAppearance.BorderSize = 10;
+            this.btnStaffLogin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.btnStaffLogin.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStaffLogin.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnStaffLogin.Location = new System.Drawing.Point(840, 841);
+            this.btnStaffLogin.Name = "btnStaffLogin";
+            this.btnStaffLogin.Size = new System.Drawing.Size(330, 94);
+            this.btnStaffLogin.TabIndex = 0;
+            this.btnStaffLogin.Text = "Staff Login";
+            this.btnStaffLogin.UseVisualStyleBackColor = false;
+            this.btnStaffLogin.Click += new System.EventHandler(this.btnStaffLogin_Click);
+            // 
+            // frm1Welcome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1623, 1177);
+            this.Controls.Add(this.btnStaffLogin);
+            this.Controls.Add(this.btnLibraryUsers);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frm1Welcome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Galaxy Libray | Welcome";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLibraryUsers;
+        private System.Windows.Forms.Button btnStaffLogin;
+    }
+}
---
+++ SA47Team10a_GalaxyLibrary/frmBossVenkatCustomer.designer.cs
@@ -0,0 +1,294 @@
+﻿namespace SA47Team10a_GalaxyLibrary
+{
+    partial class frmBossVenkatCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCustomer = new System.Windows.Forms.Button();
+            this.btnStaff = new System.Windows.Forms.Button();
+            this.btnclr = new System.Windows.Forms.Button();
+            this.btnRetrieve = new System.Windows.Forms.Button();
+            this.label13 = new System.Windows.Forms.Label();
+            this.btnUpfM = new System.Windows.Forms.Button();
+            this.txFine = new System.Windows.Forms.TextBox();
+            this.txmemberID = new System.Windows.Forms.TextBox();
+            this.lbFine = new System.Windows.Forms.Label();
+            this.lbMember = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbCategory = new System.Windows.Forms.Label();
+            this.txCategory = new System.Windows.Forms.TextBox();
+            this.lbName = new System.Windows.Forms.Label();
+            this.txName = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCustomer
+            // 
+            this.btnCustomer.BackColor = System.Drawing.Color.OliveDrab;
+            this.btnCustomer.Enabled = false;
+            this.btnCustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCustomer.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCustomer.Location = new System.Drawing.Point(789, 266);
+            this.btnCustomer.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnCustomer.Name = "btnCustomer";
+            this.btnCustomer.Size = new System.Drawing.Size(609, 62);
+            this.btnCustomer.TabIndex = 99;
+            this.btnCustomer.Text = "My Members";
+            this.btnCustomer.UseVisualStyleBackColor = false;
+            // 
+            // btnStaff
+            // 
+            this.btnStaff.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.btnStaff.Enabled = false;
+            this.btnStaff.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStaff.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnStaff.Location = new System.Drawing.Point(226, 266);
+            this.btnStaff.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnStaff.Name = "btnStaff";
+            this.btnStaff.Size = new System.Drawing.Size(560, 62);
+            this.btnStaff.TabIndex = 98;
+            this.btnStaff.Text = "My Staff";
+            this.btnStaff.UseVisualStyleBackColor = false;
+            this.btnStaff.Click += new System.EventHandler(this.btnStaff_Click);
+            // 
+            // btnclr
+            // 
+            this.btnclr.BackColor = System.Drawing.Color.OliveDrab;
+            this.btnclr.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclr.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnclr.Location = new System.Drawing.Point(597, 461);
+            this.btnclr.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnclr.Name = "btnclr";
+            this.btnclr.Size = new System.Drawing.Size(318, 69);
+            this.btnclr.TabIndex = 6;
+            this.btnclr.Text = "Clear";
+            this.btnclr.UseVisualStyleBackColor = false;
+            this.btnclr.Click += new System.EventHandler(this.btnclr_Click);
+            // 
+            // btnRetrieve
+            // 
+            this.btnRetrieve.BackColor = System.Drawing.Color.OliveDrab;
+            this.btnRetrieve.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRetrieve.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnRetrieve.Location = new System.Drawing.Point(734, 61);
+            this.btnRetrieve.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnRetrieve.Name = "btnRetrieve";
+            this.btnRetrieve.Size = new System.Drawing.Size(182, 66);
+            this.btnRetrieve.TabIndex = 1;
+            this.btnRetrieve.Text = "Retrieve";
+            this.btnRetrieve.UseVisualStyleBackColor = false;
+            this.btnRetrieve.Click += new System.EventHandler(this.btnRetrieve_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.Color.Red;
+            this.label13.Location = new System.Drawing.Point(201, 141);
+            this.label13.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(610, 33);
+            this.label13.TabIndex = 95;
+            this.label13.Text = "Notice: the format of input should be \"MXXXX\"";
+            // 
+            // btnUpfM
+            // 
+            this.btnUpfM.BackColor = System.Drawing.Color.OliveDrab;
+            this.btnUpfM.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpfM.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnUpfM.Location = new System.Drawing.Point(196, 461);
+            this.btnUpfM.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnUpfM.Name = "btnUpfM";
+            this.btnUpfM.Size = new System.Drawing.Size(345, 69);
+            this.btnUpfM.TabIndex = 5;
+            this.btnUpfM.Text = "Confirm";
+            this.btnUpfM.UseVisualStyleBackColor = false;
+            this.btnUpfM.Click += new System.EventHandler(this.btnUpfM_Click);
+            // 
+            // txFine
+            // 
+            this.txFine.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txFine.Location = new System.Drawing.Point(502, 353);
+            this.txFine.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txFine.Name = "txFine";
+            this.txFine.Size = new System.Drawing.Size(410, 44);
+            this.txFine.TabIndex = 4;
+            // 
+            // txmemberID
+            // 
+            this.txmemberID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txmemberID.Location = new System.Drawing.Point(502, 70);
+            this.txmemberID.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txmemberID.Name = "txmemberID";
+            this.txmemberID.Size = new System.Drawing.Size(196, 44);
+            this.txmemberID.TabIndex = 0;
+            this.txmemberID.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txmemberID_KeyDown);
+            // 
+            // lbFine
+            // 
+            this.lbFine.AutoSize = true;
+            this.lbFine.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbFine.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFine.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbFine.Location = new System.Drawing.Point(201, 359);
+            this.lbFine.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbFine.Name = "lbFine";
+            this.lbFine.Size = new System.Drawing.Size(83, 37);
+            this.lbFine.TabIndex = 91;
+            this.lbFine.Text = "Fine";
+            // 
+            // lbMember
+            // 
+            this.lbMember.AutoSize = true;
+            this.lbMember.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbMember.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMember.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbMember.Location = new System.Drawing.Point(201, 70);
+            this.lbMember.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbMember.Name = "lbMember";
+            this.lbMember.Size = new System.Drawing.Size(182, 37);
+            this.lbMember.TabIndex = 90;
+            this.lbMember.Text = "Member ID";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbCategory);
+            this.groupBox1.Controls.Add(this.txCategory);
+            this.groupBox1.Controls.Add(this.lbName);
+            this.groupBox1.Controls.Add(this.txName);
+            this.groupBox1.Controls.Add(this.lbMember);
+            this.groupBox1.Controls.Add(this.lbFine);
+            this.groupBox1.Controls.Add(this.btnclr);
+            this.groupBox1.Controls.Add(this.txmemberID);
+            this.groupBox1.Controls.Add(this.btnRetrieve);
+            this.groupBox1.Controls.Add(this.txFine);
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.btnUpfM);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(226, 331);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Size = new System.Drawing.Size(1170, 578);
+            this.groupBox1.TabIndex = 100;
+            this.groupBox1.TabStop = false;
+            // 
+            // lbCategory
+            // 
+            this.lbCategory.AutoSize = true;
+            this.lbCategory.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCategory.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbCategory.Location = new System.Drawing.Point(201, 284);
+            this.lbCategory.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbCategory.Name = "lbCategory";
+            this.lbCategory.Size = new System.Drawing.Size(286, 37);
+            this.lbCategory.TabIndex = 100;
+            this.lbCategory.Text = "Member Category";
+            // 
+            // txCategory
+            // 
+            this.txCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txCategory.Location = new System.Drawing.Point(502, 281);
+            this.txCategory.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txCategory.Name = "txCategory";
+            this.txCategory.ReadOnly = true;
+            this.txCategory.Size = new System.Drawing.Size(410, 44);
+            this.txCategory.TabIndex = 3;
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbName.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbName.Location = new System.Drawing.Point(201, 214);
+            this.lbName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(107, 37);
+            this.lbName.TabIndex = 98;
+            this.lbName.Text = "Name";
+            // 
+            // txName
+            // 
+            this.txName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txName.Location = new System.Drawing.Point(502, 211);
+            this.txName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txName.Name = "txName";
+            this.txName.ReadOnly = true;
+            this.txName.Size = new System.Drawing.Size(410, 44);
+            this.txName.TabIndex = 2;
+            // 
+            // frmBossVenkatCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1623, 1177);
+            this.Controls.Add(this.btnCustomer);
+            this.Controls.Add(this.btnStaff);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.Name = "frmBossVenkatCustomer";
+            this.Text = "Galaxy Library | Admin | Members";
+            this.Load += new System.EventHandler(this.frmBossVenkatCustomer_Load);
+            this.Controls.SetChildIndex(this.mbtnReport, 0);
+            this.Controls.SetChildIndex(this.mbtnBorrow, 0);
+            this.Controls.SetChildIndex(this.mbtnReturn, 0);
+            this.Controls.SetChildIndex(this.mbtnBookInformation, 0);
+            this.Controls.SetChildIndex(this.mbtnMemberInfomation, 0);
+            this.Controls.SetChildIndex(this.btnAdmin, 0);
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.groupBox1, 0);
+            this.Controls.SetChildIndex(this.btnStaff, 0);
+            this.Controls.SetChildIndex(this.btnCustomer, 0);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCustomer;
+        private System.Windows.Forms.Button btnStaff;
+        private System.Windows.Forms.Button btnclr;
+        private System.Windows.Forms.Button btnRetrieve;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button btnUpfM;
+        private System.Windows.Forms.TextBox txFine;
+        private System.Windows.Forms.TextBox txmemberID;
+        private System.Windows.Forms.Label lbFine;
+        private System.Windows.Forms.Label lbMember;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbCategory;
+        private System.Windows.Forms.TextBox txCategory;
+        private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.TextBox txName;
+    }
+}
